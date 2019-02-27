47891123995b331b3535a85acb888c86bf7be468
a-rinchan <a-chan.177002@outlook.com>
2019-02-27 21:46:58 +0900

    2/27commit

--- app/src/main/java/com/a_rin/tenki/MainActivity.java
+++ app/src/main/java/com/a_rin/tenki/MainActivity.java
@@ -1,13 +1,38 @@
 package com.a_rin.tenki;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bar_home:
+                        setFragment();
+                        return true;
+                    case R.id.bar_input:
+                        setFragment();
+                        return true;
+                    case R.id.bar_closet:
+                        setFragment();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+
 }
